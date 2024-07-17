fa3d9b272b587f4ed27eb1763b7ddcf037978f2d
Lai-ichen <myocarog333@gmail.com>
2024-07-17 23:19:37 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import Chatbot from '../components/Chatbot.vue'
+</script>
+
+<template>
+  <main>
+    <Chatbot />
+  </main>
+</template>
